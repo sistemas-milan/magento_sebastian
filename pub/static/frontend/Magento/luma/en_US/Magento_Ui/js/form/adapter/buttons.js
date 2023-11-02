a3c83e29e8e21976651ddb65d36919234aff6b32
Sistemas Milán <soporte.sap@bicicletasmilan.com>
2023-11-02 07:56:33 -0500

    install sample data

--- pub/static/frontend/Magento/luma/en_US/Magento_Ui/js/form/adapter/buttons.js
+++ pub/static/frontend/Magento/luma/en_US/Magento_Ui/js/form/adapter/buttons.js
@@ -1,17 +1,1 @@
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-/**
- * @api
- */
-define(function () {
-    'use strict';
-
-    return {
-        'reset': '#reset',
-        'save': '#save',
-        'saveAndContinue': '#save_and_continue'
-    };
-});
+/var/www/html/magento2/vendor/magento/module-ui/view/base/web/js/form/adapter/buttons.js
